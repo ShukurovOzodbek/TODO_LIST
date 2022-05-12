997a3fa0c59f55941d86e756ba2d0c1f12117955
Ozodbek_dev <99534081+ShukurovOzodbek@users.noreply.github.com>
2022-05-12 17:10:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,102 +1,78 @@
-let cont = document.querySelector('.container')
-let form = document.forms[0]
-let inputs = form.querySelectorAll('input')
-let tasks = [
-    // {
-    //     id: Math.random(),
-    //     task: 'something',
-    //     time: "10:45",
-    //     isChecked: false
-    // },
-    // {
-    //     id: Math.random(),
-    //     task: 'something',
-    //     time: "10:45",
-    //     isChecked: false
-    // },
-    // {
-    //     id: Math.random(),
-    //     task: 'something',
-    //     time: "10:45",
-    //     isChecked: false
-    // }
+let form = document.forms.todo
+let inp = form.querySelector('input')
+let todos = [
+
 ]
+
 
 form.onsubmit = (event) => {
     event.preventDefault()
-    
-    submit()
-    inputs.forEach(element => {
-        element.value = ''
-    });
+
+    let task = {
+        id: Math.random(),
+        time: `${new Date().getHours()}:${new Date().getMinutes()}`,
+        isDone: false
+    }
+
+
+    let fm = new FormData(form)
+
+    fm.forEach((value, key) => {
+        task[key] = value
+    })
+
+    if(inp.value === '') {
+
+    }else{
+        todos.push(task);
+        reload(todos)
+    }
 }
+
+let container = document.querySelector('.container')
 let no = document.querySelector('.no')
 let nothing = document.querySelector('.nothing')
 
-function submit() {
-    let user = {
-        id: Math.random()
-    }
-    
-    let fm = new FormData(form)
-    
-    fm.forEach((value, key) => {
-        user[key] = value
-    })
-    
-    tasks.push(user);
-    
-
-    inputs.forEach(element => {
-        if(element.value === '') {
-    
-        }else{
-            reload(tasks)
-        }
-    });  
-}
-
-reload(tasks)
-
-
 function reload(arr) {
-    cont.innerHTML = ""
-    
+
+    container.innerHTML = ''
+    inp.value = ''
     for(let item of arr) {
         no.style.display = 'none'
         nothing.style.display = 'none'
         let div = document.createElement('div')
         let h1 = document.createElement('h1')
+        let div2 = document.createElement('div')
         let span = document.createElement('span')
         let button = document.createElement('img')
         let input = document.createElement('input')
 
         div.classList.add('box')
-        div.setAttribute('id', item.id)
+        div2.className = 'div2'
         h1.innerHTML = item.task
         span.innerHTML = item.time
         span.classList.add('fword')
-        button.setAttribute('src', './cancel_FILL0_wght400_GRAD0_opsz48.png')
+        button.setAttribute('src', './assets/icons/cancel_FILL0_wght400_GRAD0_opsz48.png')
         button.classList.add('btn')
         input.setAttribute('type', 'checkbox')
         input.classList.add('check')
 
-        div.append(h1, span, button)
-        cont.append(div)
+        container.append(div, div2)
+        div2.append(h1, button)
+        div.append(div2, span)
         div.prepend(input)
 
         button.onclick = (e) => {
             let id = e.target.parentNode.id
 
-            let idx = tasks.findIndex(item => item.id == id)
+            let idx = todos.findIndex(item => item.id == id)
 
-            tasks.splice(idx, 1)
-
-            reload(tasks)
+            todos.splice(idx, 1)
+            reload(todos)
         }
         input.onclick = (event) => {
             let a = event.target.nextSibling
-            // a.classList.add('active')
+            console.log(a);
             if (a.classList.contains('active')) {
                 a.classList.remove('active')
                 div.style.opacity = "1"   
@@ -107,3 +83,4 @@ function reload(arr) {
         }
     }
 }
+
